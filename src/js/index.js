d24db9f8050739e4c5e98bd557e38179135228fd
parisvennis <parisvennis@gmail.com>
2020-10-20 06:43:53 +0000

    final

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,21 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let pronoun = ["the", "our", "my", "her"];
+  let adj = ["great", "big", "stupid", "nice"];
+  let noun = ["jogger", "racoon", "bike", "sandwich"];
+
+  function newDomain(pronoun, adj, noun) {
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          let domain = pronoun[i] + adj[j] + noun[k] + ".com";
+
+          console.log(domain);
+        }
+      }
+    }
+  }
+  newDomain(pronoun, adj, noun);
 };
